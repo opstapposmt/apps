52ceab38c66fe63abcc9f64f34227bbfe222e6e1
Ostap Posmt <73704205+opstapposmt@users.noreply.github.com>
2020-10-30 16:03:20 +0200

    dhdhj
    fdhjdjtyhj

--- eks/index.ts
+++ eks/index.ts
@@ -7,26 +7,11 @@ import * as eks from "@pulumi/eks";
 const config = new pulumi.Config();
 const instanceType = config.get("instanceType") as aws.ec2.InstanceType;
 const desiredCapacity = config.getNumber("desiredCapacity");
-const minSize = config.getNumber("minSize");
-const maxSize = config.getNumber("maxSize");
-const storageClass = config.get("storageClass") as eks.EBSVolumeType;
-const deployDashboard = config.getBoolean("deployDashboard");
-
-// Create a VPC for our cluster.
-const vpc = new awsx.ec2.Vpc("eksvpc", {
-    subnets: [{ type: "public" }],
-});
 
 // Create an EKS cluster with the given configuration.
 const cluster = new eks.Cluster("cluster", {
-    vpcId: vpc.id,
-    subnetIds: vpc.publicSubnetIds,
     instanceType: instanceType,
-    desiredCapacity: desiredCapacity,
-    minSize: minSize,
-    maxSize: maxSize,
-    storageClasses: storageClass,
-    deployDashboard: deployDashboard,
+    desiredCapacity: desiredCapacity
 });
 
 // Export the cluster's kubeconfig.
